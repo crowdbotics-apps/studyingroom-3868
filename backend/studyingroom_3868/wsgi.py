61e8cfb7f7e2d4ffd44a74df7fe32d3ea4b5a7ab
Crowdbotics <team@crowdbotics.com>
2019-05-26 19:35:42 +0000

    initial commit

---
+++ backend/studyingroom_3868/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for studyingroom_3868 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studyingroom_3868.settings")
+
+application = get_wsgi_application()
